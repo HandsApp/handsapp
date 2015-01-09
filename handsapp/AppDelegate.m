334965fad5eeb30dc7dddeafb05a8fe669a9bcb7
Tito <albertesga@gmail.com>
2015-01-09 10:08:21 +0100

    ok

--- handsapp/AppDelegate.m
+++ handsapp/AppDelegate.m
@@ -18,6 +18,8 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
     NSLog(@"hola");
+    
+    NSLog(@"adios");
     return YES;
 }
 
